c177e9bcb0c46ea3d2190983748554bdc13d663d
Observer <80362048+68756d616e@users.noreply.github.com>
2023-01-24 22:51:06 +0000

    Update # Calculator.py

--- # Calculator.py
+++ # Calculator.py
@@ -27,7 +27,7 @@ print("""
 while True:
     
     type = input("""Welcome to a multi method calculator
-You can choose one of the following calculators
+You can choose one of the following calculators - When chosen, the additional options related to the type of calculator will populate.
 A - Percentage Calculator
 B - Volume Calculator
 C - Random Number Generator
@@ -48,14 +48,37 @@ Steps:
         percentage = float(input(f"Please type the percentage (example, type 0.25 for 25%): %"))
         number = float(input(f"You would like %{percentage} of what number? :"))
 
-        # The calculation - Find a method to change the input 25 into 0.25 - for now request it as an input
+        # The calculation - Find a method to change the input 25 into 0.25 - for now request it as an decimal input
         calculation = percentage * number 
         print(f"Your answer: %{percentage} of {number} is {calculation}")
 
 
-# If the yser chose B = A 
+# If the user chose B
     elif type == 'B' or type == 'b':
         print("You chose a Volume Calculator")
+
+# List of options the user can choose related to volume, once the user choose a type, the list of conversions will display.
+        print() # Empty Space
+        volume_choice = input("""Here are your options! Once you choose, your list of conversions will populate
+A - Sphere Volume Calculator
+B - Cone Volume Calculator
+C - Cube Volume Calculator
+D - Cylinder Volume Calculator
+E - Rectangular Tank Volume Calculator
+F - Capsule Volume Calculator
+G - Spherical Cap Volume Calculator
+H - Conical Frustum Volume Calculator
+I - Ellipsoid Volume Calculator
+J - Square Pyramid Volume Calculator
+K - Tube Volume Calculator
+Please tpye your answer here: """)
+        if volume_choice == 'a':
+            print("This works")
+        
+
+        # Option B
+        elif volume_choice == 'b' or volume_choice 'B':
+            print("")
 
 # If the user chose C - A Random Number Generator, they will be provided a definition and a calculation for their input    
     elif type == 'C' or type == 'c':
